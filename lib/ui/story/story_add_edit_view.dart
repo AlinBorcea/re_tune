0e816da658e5bd7eae83d7c6ec3ac93a8b689434
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2025-10-27 09:40:14 +0200

    added form to insert and update story

---
+++ lib/ui/story/story_add_edit_view.dart
@@ -0,0 +1,108 @@
+import 'package:date_field/date_field.dart';
+import 'package:flutter/material.dart';
+import 'package:re_tune/domain/models/story/story.dart';
+import 'package:re_tune/ui/story/story_view_model.dart';
+
+class StoryViewAddEdit extends StatefulWidget {
+  const StoryViewAddEdit({
+    super.key,
+    required this.storyViewModel,
+    required this.story,
+  });
+
+  final StoryViewModel storyViewModel;
+  final Story? story;
+
+  @override
+  State<StatefulWidget> createState() => _StoryViewAddEditState();
+}
+
+class _StoryViewAddEditState extends State<StoryViewAddEdit> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _descriptionController = TextEditingController();
+  DateTime? _startDate, _endDate;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.story == null) return;
+
+    _nameController.text = widget.story!.name ?? '';
+    _descriptionController.text = widget.story!.description ?? '';
+    _startDate = widget.story!.startDate;
+    _endDate = widget.story!.endDate;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Add Story')),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: 'Story Name',
+                  hintText: 'Start a great story!',
+                ),
+                validator: (value) =>
+                    widget.storyViewModel.isValidStoryName(value)
+                    ? null
+                    : 'Name is not valid!',
+              ),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: InputDecoration(
+                  labelText: 'Story Description',
+                  hintText: 'Tell everyone more about it!',
+                ),
+                validator: (value) =>
+                    widget.storyViewModel.isValidStoryDescription(value)
+                    ? null
+                    : 'Description must be at least 5 characters long',
+              ),
+              DateTimeFormField(
+                decoration: InputDecoration(labelText: 'Enter start date'),
+                initialPickerDateTime: _startDate,
+                onChanged: (value) {
+                  _startDate = value ?? _startDate;
+                },
+              ),
+              DateTimeFormField(
+                decoration: InputDecoration(labelText: 'Enter end date'),
+                initialPickerDateTime: _endDate,
+                onChanged: (value) {
+                  _endDate = value ?? _endDate;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  bool isValid1 = _formKey.currentState!.validate();
+                  bool isValid2 = widget.storyViewModel.isValidStoryDates(
+                    _startDate,
+                    _endDate,
+                  );
+                  debugPrint('Validation button: $isValid1 and $isValid2');
+                  final story = Story()
+                    ..name = _nameController.text
+                    ..description = _descriptionController.text
+                    ..startDate = _startDate
+                    ..endDate = _endDate;
+
+                  if (widget.story != null) story.id = widget.story!.id;
+                  widget.storyViewModel.putStory(story);
+                  Navigator.of(context).pop();
+                },
+                child: Text('Save'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
