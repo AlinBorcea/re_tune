79d52b858b5995c9e708701fc7f7e51069e90c2d
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2025-10-30 14:28:40 +0200

    added date and time pickers

--- lib/ui/settings/time_settings.dart
+++ lib/ui/settings/time_settings.dart
@@ -1,3 +1,4 @@
+import 'package:date_field/date_field.dart';
 import 'package:flutter/material.dart';
 
 class TimeSettings extends StatefulWidget {
@@ -8,6 +9,10 @@ class TimeSettings extends StatefulWidget {
 }
 
 class _TimeSettingsState extends State<TimeSettings> {
+  final _dailyHourController = TextEditingController();
+  final _weeklyDayController = TextEditingController();
+  final _monthlyDayController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,31 +22,85 @@ class _TimeSettingsState extends State<TimeSettings> {
           child: Column(
             children: [
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Flexible(child: Text('Daily Hour to report: ')),
                   Flexible(
                     child: TextField(
-                      decoration: InputDecoration(labelText: 'Time of day'),
+                      controller: _dailyHourController,
+                      decoration: InputDecoration(
+                        labelText: _dailyHourController.text,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                          borderSide: BorderSide(color: Colors.black, width: 1),
+                        ),
+                      ),
+                      onTap: () async {
+                        final time = await showAdaptiveDateTimePicker(
+                          context: context,
+                          mode: DateTimeFieldPickerMode.time,
+                        );
+
+                        if (time != null) {
+                          _dailyHourController.text = time.toString();
+                        }
+                      },
                     ),
                   ),
                 ],
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Flexible(child: Text('Weekly Day to report')),
                   Flexible(
                     child: TextField(
-                      decoration: InputDecoration(labelText: 'Day of week'),
+                      controller: _weeklyDayController,
+                      decoration: InputDecoration(
+                        labelText: _weeklyDayController.text,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                          borderSide: BorderSide(color: Colors.black, width: 1),
+                        ),
+                      ),
+                      onTap: () async {
+                        final time = await showAdaptiveDateTimePicker(
+                          context: context,
+                          mode: DateTimeFieldPickerMode.date,
+                        );
+
+                        if (time != null) {
+                          _weeklyDayController.text = time.toString();
+                        }
+                      },
                     ),
                   ),
                 ],
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Flexible(child: Text('Monthly Day to report')),
                   Flexible(
                     child: TextField(
-                      decoration: InputDecoration(labelText: 'Day of month'),
+                      controller: _monthlyDayController,
+                      decoration: InputDecoration(
+                        labelText: _monthlyDayController.text,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                          borderSide: BorderSide(color: Colors.black, width: 1),
+                        ),
+                      ),
+                      onTap: () async {
+                        final time = await showAdaptiveDateTimePicker(
+                          context: context,
+                          mode: DateTimeFieldPickerMode.date,
+                        );
+
+                        if (time != null) {
+                          _monthlyDayController.text = time.toString();
+                        }
+                      },
                     ),
                   ),
                 ],
