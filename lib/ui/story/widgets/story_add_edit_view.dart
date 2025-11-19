0a4cbcc0fd127f6d5d7a46f84288272f8c8708f1
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2025-11-19 10:03:00 +0200

    modified layout

--- lib/ui/story/widgets/story_add_edit_view.dart
+++ lib/ui/story/widgets/story_add_edit_view.dart
@@ -43,128 +43,134 @@ class _StoryViewAddEditState extends State<StoryViewAddEdit> {
         title: Text(widget.story != null ? widget.story!.name! : 'Add Story'),
       ),
       body: Center(
-        child: Card(
-          margin: EdgeInsets.all(8),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _nameController,
-                  decoration: InputDecoration(
-                    labelText: 'Story Name',
-                    hintText: 'Start a great story!',
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                      borderSide: BorderSide(color: Colors.black, width: 1),
-                    ),
-                  ),
-                  validator: (value) =>
-                      widget.storyViewModel.isValidStoryName(value)
-                      ? null
-                      : 'Name is not valid!',
-                ),
-                SizedBox(height: 10,),
-                TextFormField(
-                  controller: _descriptionController,
-                  decoration: InputDecoration(
-                    labelText: 'Story Description',
-                    hintText: 'Tell everyone more about it!',
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                      borderSide: BorderSide(color: Colors.black, width: 1),
-                    ),
-                  ),
-                  validator: (value) =>
-                      widget.storyViewModel.isValidStoryDescription(value)
-                      ? null
-                      : 'Description must be at least 5 characters long',
-                ),
-                SizedBox(height: 10,),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextFormField(
-                        controller: _startDateController,
-                        decoration: InputDecoration(
-                          labelText: 'Enter start date',
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(4.0),
-                            ),
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                              width: 1,
-                            ),
-                          ),
-                        ),
-                        onTap: () async {
-                          final pickedDate = await showAdaptiveDateTimePicker(
-                            context: context,
-                            mode: DateTimeFieldPickerMode.date,
-                          );
-                          if (pickedDate != null) {
-                            _startDate = pickedDate;
-                            _startDateController.text = _startDate.toString();
-                            setState(() {});
-                          }
-                        },
+        child: SizedBox(
+          width: 600,
+          height: 230,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusGeometry.circular(4),
+            ),
+            elevation: 4,
+            margin: EdgeInsets.all(8),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                spacing: 8,
+                children: [
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      labelText: 'Story Name',
+                      hintText: 'Start a great story!',
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                        borderSide: BorderSide(color: Colors.black, width: 1),
                       ),
                     ),
-                    SizedBox(width: 4),
-                    Expanded(
-                      child: TextFormField(
-                        controller: _endDateController,
-                        decoration: InputDecoration(
-                          labelText: 'Enter end date',
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(4.0),
-                            ),
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                              width: 1,
-                            ),
-                          ),
-                        ),
-                        onTap: () async {
-                          final pickedDate = await showAdaptiveDateTimePicker(
-                            context: context,
-                            mode: DateTimeFieldPickerMode.date,
-                          );
-
-                          if (pickedDate != null) {
-                            _endDate = pickedDate;
-                            _endDateController.text = _endDate.toString();
-                            setState(() {});
-                          }
-                        },
+                    validator: (value) =>
+                        widget.storyViewModel.isValidStoryName(value)
+                        ? null
+                        : 'Name is not valid!',
+                  ),
+                  TextFormField(
+                    controller: _descriptionController,
+                    decoration: InputDecoration(
+                      labelText: 'Story Description',
+                      hintText: 'Tell everyone more about it!',
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                        borderSide: BorderSide(color: Colors.black, width: 1),
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(height: 4,),
-                ElevatedButton(
-                  onPressed: () async {
-                    bool isValid1 = _formKey.currentState!.validate();
-                    bool isValid2 = widget.storyViewModel.isValidStoryDates(
-                      _startDate,
-                      _endDate,
-                    );
-                    debugPrint('Validation button: $isValid1 and $isValid2');
-                    final story = Story()
-                      ..name = _nameController.text
-                      ..description = _descriptionController.text
-                      ..startDate = _startDate
-                      ..endDate = _endDate;
+                    validator: (value) =>
+                        widget.storyViewModel.isValidStoryDescription(value)
+                        ? null
+                        : 'Description must be at least 5 characters long',
+                  ),
+                  Row(
+                    spacing: 4,
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          controller: _startDateController,
+                          decoration: InputDecoration(
+                            labelText: 'Enter start date',
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(4.0),
+                              ),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                                width: 1,
+                              ),
+                            ),
+                          ),
+                          onTap: () async {
+                            final pickedDate = await showAdaptiveDateTimePicker(
+                              context: context,
+                              mode: DateTimeFieldPickerMode.date,
+                            );
+                            if (pickedDate != null) {
+                              _startDate = pickedDate;
+                              _startDateController.text = _startDate.toString();
+                              setState(() {});
+                            }
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          controller: _endDateController,
+                          decoration: InputDecoration(
+                            labelText: 'Enter end date',
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(4.0),
+                              ),
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                                width: 1,
+                              ),
+                            ),
+                          ),
+                          onTap: () async {
+                            final pickedDate = await showAdaptiveDateTimePicker(
+                              context: context,
+                              mode: DateTimeFieldPickerMode.date,
+                            );
 
-                    if (widget.story != null) story.id = widget.story!.id;
-                    await widget.storyViewModel.putStory(story);
-                    //Navigator.of(context).pop();
-                  },
-                  child: Text('Save'),
-                ),
-              ],
+                            if (pickedDate != null) {
+                              _endDate = pickedDate;
+                              _endDateController.text = _endDate.toString();
+                              setState(() {});
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      bool isValid1 = _formKey.currentState!.validate();
+                      bool isValid2 = widget.storyViewModel.isValidStoryDates(
+                        _startDate,
+                        _endDate,
+                      );
+                      debugPrint('Validation button: $isValid1 and $isValid2');
+                      final story = Story()
+                        ..name = _nameController.text
+                        ..description = _descriptionController.text
+                        ..startDate = _startDate
+                        ..endDate = _endDate;
+
+                      if (widget.story != null) story.id = widget.story!.id;
+                      await widget.storyViewModel.putStory(story);
+                      //Navigator.of(context).pop();
+                    },
+                    child: Text('Save'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
